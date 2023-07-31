e9b4191ae448364c8b38cc6679c3315e88aa370e
dinglijun052129 <3101857465@qq.com>
2023-07-31 17:22:17 +0800

    Changes

--- Start_csyl/Web_test.py
+++ Start_csyl/Web_test.py
@@ -1,6 +1,10 @@
 import pytest
 import requests
 def web_login():
+    """
+    A+WEB：登录接口
+    :return:
+    """
     url='https://cvstest01.info-plus.cn/KIT/homePage/doLogin'
     data={
      "userName":"13839990000",
@@ -9,6 +13,25 @@ def web_login():
     req=requests.post(url=url,data=data)
     print(req.status_code)
     print(req.text)
-
+def T2001Ajax(params=None):
+    """
+    A+WEB：分类月别构成分析（旧）
+    :return:
+    """
+    url='https://cvstest01.info-plus.cn/KIT/T2001/T2001Ajax?'
+    params={
+        "select":" 0",
+        "date":"",
+        "order":"",
+        "categoryId":"",
+        "categoryLevel":"1",
+        "storegroup_code":"",
+        "storeId:164":""
+    }
+    Cookie={"SESSION":"MDFlMzJhNGItMzhiZi00ODAwLWEzODktNTY5YTliMjJlYTQ1"}
+    req=requests.get(url=url,params=params,cookies=Cookie)
+    print(req.status_code)
+    print(req.json())
 if __name__ == '__main__':
     web_login()
+    T2001Ajax()
